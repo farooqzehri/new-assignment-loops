048cd8236b2eab4fdbd2c3b9941bc57355ce028b
farooqzehri <farooqahmedbaloch17@gmail.com>
2025-11-06 01:16:22 +0500

    Question no 2 complete

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const fruits = ["mango" , "banana" , "orange" , "Kiwi" , "watermelon" , "grapes"]
+// 1. Create an array of fruits like ["apple", "banana", "cherry"] and use a loop to log each fruit 
+// in the console. 
+
+// const fruits = ["mango" , "banana" , "orange" , "Kiwi" , "watermelon" , "grapes"]
+// for(let i = 0 ; i < fruits.length; i++){
+//     console.log(fruits[i],i);
+    
+// }
+
+// q no 1     ok
+
+
+// 2. Given an array of colors ["red", "green", "blue"], use a loop to add all the color names to 
+// a paragraph with the ID output using document.querySelector.
+
+
+let colors = ["red" , "green" , "blue" , "yellow"];
+const paragraph =document.querySelector("#input");
+for(let i = 0; i < colors.length; i++){
+    paragraph.innerHTML += colors[i] + ". ";
+} 
